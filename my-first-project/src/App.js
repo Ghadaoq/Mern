6d93568b7b56345bba8f06e41de468890d951cfc
Ghadaoq <74109188+Ghadaoq@users.noreply.github.com>
2021-01-29 21:08:35 +0200

    firstjsx

---
+++ my-first-project/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo !</h1>
+      <h3> things i need to do:</h3>
+      <ul>
+          <li>Learn React</li>
+          <li>Climb Mount Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the Dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
