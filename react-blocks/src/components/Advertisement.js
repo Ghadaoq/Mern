c4897f7e95afdda1135c22aef661841900e95860
Ghadaoq <74109188+Ghadaoq@users.noreply.github.com>
2021-01-30 15:42:42 +0200

    css

---
+++ react-blocks/src/components/Advertisement.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+// const adiStyle = {
+//     minheight: '120px',
+//     width: '660px',
+//     backgroundcolor: 'purple',
+//     margin: '10px'
+// };
+export class Advertisement extends Component {
+    render() {
+        return (
+            <div className="adi">
+                
+            </div>
+        )
+    }
+}
+
+export default Advertisement
---
+++ react-blocks/src/components/SubContents.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+// const childStyle = {
+//     width: '200px',
+//     backgroundcolor: 'yellow',
+//     display:'inline-block',
+// };
+
+export class SubContents extends Component {
+    render() {
+        return (
+            <div className="child">
+            </div>
+        )
+    }
+}
+
+export default SubContents
